e7b03cf0335349e394d7fcddec6049f2daef7cad
Jury Verrigni <jury.verrigni@gmail.com>
2017-01-06 10:08:32 +0000

    First commit

---
+++ conf/default.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Default settings for the authjoomla3 plugin
+ *
+ * @author Jury Verrigni <jury.verrigni@skayahack.it>
+ */
+
+$conf['joomlaPath'] = ''; // Absolute path to Joomla root
---
+++ conf/metadata.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Options for the authjoomla3 plugin
+ *
+ * @author Jury Verrigni <jury.verrigni@skayahack.it>
+ */
+
+$meta['joomlaPath'] = array('string');
