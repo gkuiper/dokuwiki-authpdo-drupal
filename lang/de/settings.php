043c51e865f05733bca7904a9de55470cb5225b7
Christoph Ziehr <christoph@christoph-ziehr.de>
2017-01-06 14:48:52 +0100

    German language file added

---
+++ lang/de/settings.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * German language file for authjoomla3 plugin
+ *
+ * @author Christoph Ziehr <info@einsatzleiterwiki.de>
+ */
+$lang['joomlaPath'] = 'Absoluter Pfad zur Joomla 3-Installation';
