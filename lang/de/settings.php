2fd8919419b52657366d67aaa685814efa7f5f5a
Guus Kuiper <guuskuiper@gmail.com>
2018-12-23 20:01:02 +0100

    Remove lang/de

--- lang/de/settings.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * German language file for authjoomla3 plugin
- *
- * @author Christoph Ziehr <info@einsatzleiterwiki.de>
- */
- 
-$lang['joomlaPath'] = 'Absoluter Pfad zur Joomla 3-Installation';
